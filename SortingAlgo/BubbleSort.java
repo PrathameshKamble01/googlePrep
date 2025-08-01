1bf81230bc3445f6ee3827a9f77a7f5e887340a7
pk315 <pk315@enovate-it.com>
2025-08-01 13:10:18 +0530

    Update .gitignore and remove ignored files

--- SortingAlgo/BubbleSort.java
+++ SortingAlgo/BubbleSort.java
@@ -1,6 +1,8 @@
+package SortingAlgo;
+
 class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
         bubbleSort(arr);
         System.out.println("Sorted array: ");
         for (int num : arr) {
@@ -11,15 +13,15 @@ class BubbleSort {
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
         // Traverse through all array elements
-        for (int i = n-1; i>1; i--) {
-            for(int j=0; j<i; j++){
-                if(arr[j] > arr[i]){
-                    swap(arr, j, i);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < i ; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
                 }
             }
+        }
     }
-}
-    
+
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
--- SortingAlgo/SelectionSort.java
+++ SortingAlgo/SelectionSort.java
@@ -1,3 +1,5 @@
+package SortingAlgo;
+
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
